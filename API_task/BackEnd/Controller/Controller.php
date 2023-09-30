2e9ec60aa639c2b5be6ce18e39a89c54fd902e47
shaileshprajapati1 <shaileshprajapati966@gmail.com>
2023-09-30 11:46:20 +0530

    data

--- API_task/BackEnd/Controller/Controller.php
+++ API_task/BackEnd/Controller/Controller.php
@@ -1,26 +1,24 @@
-<?php 
+<?php
 require_once("Model/Model.php");
-class Controller extends Model{
+class Controller extends Model
+{
 
     function __construct()
     {
         parent::__construct();
-       if(isset($_SERVER['PATH_INFO'])){
-        switch ($_SERVER['PATH_INFO']) {
-            case '/alldata':
-                echo "alldata";
-                break;
-            
-            default:
-                # code...
-                break;
+        if (isset($_SERVER['PATH_INFO'])) {
+            switch ($_SERVER['PATH_INFO']) {
+                case '/alldata':
+                    $AllData = $this->Select("users");
+                    echo json_encode($AllData);
+                    break;
+
+                default:
+                    # code...
+                    break;
+            }
         }
-       }
     }
-
-
 }
 
 $Controller = new Controller;
-
-?>
--- API_task/BackEnd/Model/Model.php
+++ API_task/BackEnd/Model/Model.php
@@ -13,7 +13,31 @@ class Model
         // }
     }
 
-    function Select($tbl,$where=null){
-        
+    function Select($tbl, $where = null)
+    {
+        $SQL = " SELECT * FROM $tbl";
+        if ($where != null || $where != "") {
+            $SQL .= " WHERE";
+            foreach ($where as $key => $value) {
+                $SQL .= " $key = '$value' AND";
+            }
+            $SQL = rtrim($SQL, "AND");
+        }
+        // echo $SQL;
+        $SQLEx = $this->connection->query($SQL);
+        if ($SQLEx->num_rows > 0) {
+            while ($data = $SQLEx->fetch_object()) {
+                $FetchData[] = $data;
+            }
+
+            $Responce['Code'] = "1";
+            $Responce['Msg'] = "Success";
+            $Responce['Data'] = $FetchData;
+        } else {
+            $Responce['Data'] = "0";
+            $Responce['Code'] = "0";
+            $Responce['Msg'] = "Try Again";
+        }
+        return $Responce;
     }
 }
